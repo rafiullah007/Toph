bc5dce416e23dbb5dd9ce4ca4ec3ba552b9a7865
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-26 20:46:05 +0600

    Update Add Them Up.cpp

--- Add Them Up.cpp
+++ Add Them Up.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
 int sum (int a, int b)
 {
 	return a+b;
