0645374cdaa5a97d132ffb72827fdd3cbbba1c29
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-01-26 21:30:09 +0600

    Add files via upload

---
+++ Divisors.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+int main()
+{
+	int a;
+	cin>>a;
+	for(int i = 1; i<=a ; i++)
+    {
+        if(a%i==0)
+        {
+            cout<<i<<endl;
+        }
+    }
+}
+
+
