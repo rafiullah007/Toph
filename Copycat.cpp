774d0c7af0b4153e27d0ea7f0aa1771ccff0550a
Rafiullah Al Naim <136167854+rafiullah007@users.noreply.github.com>
2024-03-24 02:02:09 +0600

    Add files via upload

---
+++ Copycat.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    /*
+
+    MD. RAFIULLAH AL NAIM
+    UNIVERSITY : UNIVERSITY OF ASIA PACIFIC
+    STUDENT ID : 22101150 (UAP,DHAKA,BANGLADESH)
+    COLLEGE : ADAMJEE CANTONMENT COLLEGE
+    STUDENT ID : 181063 (ACC,DHAKA,BANGLADESH)
+
+    */
+
+ int main()
+ {
+     int n;
+     cin>>n;
+     cout<<n<<endl;
+ }
